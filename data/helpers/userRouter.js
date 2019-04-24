1b450b05dde562000cea7e3da6e25a50369a9f59
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-04-24 17:13:32 -0400

    Created endpoint for DELETE request when deleting user from the database

--- data/helpers/userRouter.js
+++ data/helpers/userRouter.js
@@ -27,7 +27,7 @@ router.get('/:id', (req, res) => {
                 res.json(user);
             } else {
                 res.status(404).json({
-                    message: "The user with the specified ID does not exist"
+                    message: "The user with the specified ID does not exist."
                 });
             }
         })
@@ -53,5 +53,28 @@ router.post('/', (req, res) => {
             })
         });
 });
+
+router.delete('/:id', (req, res) => {
+    const userId = req.params.id;
+    Users
+        .remove(userId)
+        .then(deletedUser => {
+            if (deletedUser) {
+                res.status(201).json({
+                    message: "The user was deleted."
+                });
+            } else {
+                res.status(404).json({
+                    message: "The user with the specified ID does not exist."
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err,
+                message: "The user could not be deleted."
+            })
+        })
+})
 
 module.exports = router;
