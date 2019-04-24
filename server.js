574baef064e4ed09d43d0bee03398ae38fe22946
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-04-24 16:50:02 -0400

    Installed helmet and morgan middleware dependencies

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan')
 
 const server = express();
 
 const usersRouter = require('./data/helpers/userRouter');
 
 server.use(express.json());
+server.use(helmet());
+server.use(morgan('dev'));
 
 server.get('/', (req, res) => {
     res.send('Ready to go');
